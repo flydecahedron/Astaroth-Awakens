edae5e2a61ea395cbdd85a42b8e8adc56d2a5df1
flydecahedron <eschmdbrg@gmail.com>
2016-06-28 16:24:38 -0400

    Removed unused assets and libs. Organized shaders
    Added shaders folder and moved 'selected' fragment shader out of main
    and into shaders. Removed unused files

--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 -- load libs
-local camera = require "libs/hump.camera"
 
+-- load assets
+--local tiles = require "tileset"
+
+-- load shader
 function love.load()
 	--camera = Camera(0, 0)
 
@@ -27,124 +30,8 @@ function love.load()
 	{4,5,6},
 	{7,8,9}
 	}
-	--gradient shader over time on "foreground" or ascii character
-	shader1 = love.graphics.newShader(
-  [[
-    extern vec4 palette_colors[2]; // size of color palette (2 colors)
-    extern number dt, quadX, quadY, quadW, quadH, tilesetW, tilesetH;
-    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
-      //vec2 quad_coords = vec2((texture_coords.x * tilesetW - quadX) / quadW,(texture_coords.y * tilesetH - quadY) / quadH);
-      if(pixel == vec4(1.0,1.0,1.0,1.0)){
-      	pixel = vec4(1.0,0.0,0.0,1.0);
-	  	number average = (pixel.r+pixel.b+pixel.g)/3.0;
-	  	number factorX = (texture_coords.x * tilesetW - quadX) / quadW;
-	  	factorX = factorX + dt;
-	  	//number factorY = (texture_coords.y * tilesetH - quadY) / quadH;
-	  	pixel.r = pixel.r + (average-pixel.r) * factorX;
-	  	//pixel.g = pixel.g + (average-pixel.g) * factorX;
-	  	//pixel.b = pixel.b + (average-pixel.b) * factorX;
-      	return pixel;
-      }
-      if(pixel == vec4(1.0,0.0,1.0,1.0)){
-      	return vec4(0.0,0.0,0.0,0.0);
-      }
-      return pixel * color;
-    }
 
-  ]] )
-	--gradient shader over time on "background"
-	shader2 = love.graphics.newShader(
-  [[
-    extern vec4 palette_colors[2]; // size of color palette (2 colors)
-    extern number dt, quadX, quadY, quadW, quadH, tilesetW, tilesetH, offset;
-    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
-      //vec2 quad_coords = vec2((texture_coords.x * tilesetW - quadX) / quadW,(texture_coords.y * tilesetH - quadY) / quadH);
-      if(pixel == vec4(1.0,1.0,1.0,1.0)){
-      	//pixel = vec4(1.0,0.0,0.0,1.0);
-      	return pixel;
-      }
-      if(pixel == vec4(1.0,0.0,1.0,1.0)){
-      	pixel = vec4(0.0,0.7,0.0,0.8);
-	  	number quadCoordX = (texture_coords.x * tilesetW - quadX) / quadW;
-	  	number factorX = quadCoordX + dt;
-	  	number quadCoordY = (texture_coords.y * tilesetH - quadY) / quadH;
-	  	number factorY = (quadCoordY + 2) * dt;
-	  	//pixel.r = pixel.r + (average-pixel.r) * factorX;
-	  	pixel.g = pixel.g * factorX * factorY;
-	  	//pixel.b = pixel.b + (average-pixel.b) * factorX;
-      	return pixel;
-      }
-      return pixel * color;
-    }
-  ]] )
-	--gradient bar moves over background
-	-- use an offset variable against the y coord ?
-	shader3 = love.graphics.newShader(
-  [[
-    extern vec4 palette_colors[2]; // size of color palette (2 colors)
-    extern number dt, quadX, quadY, quadW, quadH, tilesetW, tilesetH, offset;
-    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
-      //vec2 quad_coords = vec2((texture_coords.x * tilesetW - quadX) / quadW,(texture_coords.y * tilesetH - quadY) / quadH);
-      if(pixel == vec4(1.0,1.0,1.0,1.0)){
-      	//pixel = vec4(1.0,0.0,0.0,1.0);
-      	return pixel;
-      }
-      if(pixel == vec4(1.0,0.0,1.0,1.0)){
-	  	number quadCoordX = (texture_coords.x * tilesetW - quadX) / quadW;
-	  	//number factorX = quadCoordX + dt;
-	  	number quadCoordY = (texture_coords.y * tilesetH - quadY) / quadH;
-	  	number factorY = (quadCoordY + 2) * dt;
-	  	number centerY = quadH/2;
-	  	number gradientH = quadH/10;
-	  	if(quadCoordY>centerY){
-	  		if(quadCoordY<(centerY + gradientH)){
-	  			pixel.r = 0.0;
-			  	pixel.g = 1.0 - (quadCoordY) * dt;
-			  	pixel.b = 0.0;
-			  	//pixel.a = quadCoordX * dt;
-		      	return pixel;
-	  		}
-	  	}
-	  	pixel.r = 0.0;
-	  	pixel.g = 1.0 - (quadCoordY) * dt;
-	  	pixel.b = 0.0;
-	  	//pixel.a = quadCoordX * dt;
-      	return pixel;
-      }
-      return pixel * color;
-    }
-  ]] )
-
-		shader = love.graphics.newShader(
-  [[
-    extern vec4 palette_colors[2]; // size of color palette (2 colors)
-    extern number dt, quadX, quadY, quadW, quadH, tilesetW, tilesetH, offset;
-    vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
-      vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
-      vec2 quad_coords = vec2((texture_coords.x * tilesetW - quadX) / quadW,(texture_coords.y * tilesetH - quadY) / quadH);
-      if(pixel == vec4(1.0,1.0,1.0,1.0)){
-      	//pixel = vec4(1.0,0.0,0.0,1.0);
-      	return pixel;
-      }
-      if(pixel == vec4(1.0,0.0,1.0,1.0)){
-	  	//number quadCoordX = (texture_coords.x * tilesetW - quadX) / quadW;
-	  	//number factorX = quadCoordX + dt;
-	  	//number quadCoordY = (texture_coords.y * tilesetH - quadY) / quadH;
-	  	//number factorY = (quadCoordY + 2) * dt;
-	  	//vec2 quadCenter = vec2((quadW/2 - quadX)/tilesetW,((tilesetH - quadY) / quadH)/2);
-	  	vec2 quadCenter = vec2(0.5,0.5);
-	  	vec2 quadDistance = (quad_coords - quadCenter);
-	  	number green = 0.7 - length(quadDistance);
-	  	number red = 0.7- length(quadDistance);
-	  	pixel = vec4(red,green,0.0,0.8 * (dt));
-      	return pixel;
-      }
-      return pixel * color;
-    }
-  ]] )
+	shader = love.graphics.newShader("shaders/selected.frag")
 	shader:send("quadW", tileW)
 	shader:send("quadH", tileH)
 	shader:send("tilesetW", tilesetW)
@@ -173,7 +60,7 @@ function love.update(dt)
 		else
 		    time = 0
 		    time0 = 1
-			shader:send("dt",time)		    
+			shader:send("dt",time)
 		end
 	end
 end
---
+++ shaders/selected.frag
@@ -0,0 +1,22 @@
+
+//extern vec4 palette_colors[2]; // size of color palette (2 colors)
+extern number dt, quadX, quadY, quadW, quadH, tilesetW, tilesetH, offset;
+vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+  vec4 pixel = Texel(texture, texture_coords );//This is the current pixel color
+  vec2 quad_coords = vec2((texture_coords.x * tilesetW - quadX) / quadW,(texture_coords.y * tilesetH - quadY) / quadH);
+  if(pixel == vec4(1.0,1.0,1.0,1.0)){
+    return pixel;
+  }
+  //pulse background with a gold/brown gradient
+  else if(pixel == vec4(1.0,0.0,1.0,1.0)){
+  vec2 quadCenter = vec2(0.5,0.5);
+  vec2 quadDistance = (quad_coords - quadCenter);
+  number green = 0.7 - length(quadDistance);
+  number red = 0.7- length(quadDistance);
+  pixel = vec4(red,green,0.0,0.8 * (dt));
+    return pixel;
+  }
+  else{
+      return pixel * color;
+  }
+}
